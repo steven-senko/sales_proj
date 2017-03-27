1e1c65975c57e4dd77482b12137010c691aacb33
Steven Senko <steven-senko@users.noreply.github.com>
2017-03-26 23:57:03 -0400

    Sales service ready to run

---
+++ sales_service.py
@@ -0,0 +1,223 @@
+import json
+
+import cherrypy
+from sqlalchemy import create_engine
+
+SECURE_STRING = "fm7m67u44p"
+DB_STRING = "sale.db"
+_engine = create_engine('sqlite:///sale.db', echo=True)
+
+
+@cherrypy.expose
+class Customer(object):
+    @cherrypy.tools.accept(media='text/plain')
+    @cherrypy.tools.json_out()
+    def PUT(self):
+        cl = cherrypy.request.headers['Content-Length']
+        raw_body = cherrypy.request.body.read(int(cl))
+        data = json.loads(raw_body)
+        res = {
+            'success': False,
+            'error': "",
+            'data': data,
+            'id': None
+        }
+        conn = _engine.connect()
+        insert = conn.execute(
+            "INSERT INTO customer VALUES ('{0}', '{1}')".format(data['first_name'], data['last_name']))
+        insert_id = insert.lastrowid
+        if insert_id:
+            res['success'] = True
+            res['id'] = insert_id
+        else:
+            res['error'] = "Cannot insert data"
+        conn.close()
+
+        return res
+
+    @cherrypy.tools.accept(media='text/plain')
+    @cherrypy.tools.json_out()
+    def DELETE(self, customer_id):
+        res = {
+            'success': False,
+            'error': "",
+            'data': customer_id
+        }
+
+        conn = _engine.connect()
+        conn.execute("DELETE FROM cart WHERE customer_id='{0}'".format(customer_id))
+        n = conn.execute("DELETE FROM customer WHERE rowid='{0}'".format(customer_id)).rowcount
+        if n:
+            res['success'] = True
+        else:
+            res['error'] = "Can not delete"
+        conn.close()
+
+        return res
+
+
+@cherrypy.expose
+class SKU(object):
+    @cherrypy.tools.accept(media='text/plain')
+    @cherrypy.tools.json_out()
+    def PUT(self):
+        cl = cherrypy.request.headers['Content-Length']
+        raw_body = cherrypy.request.body.read(int(cl))
+        data = json.loads(raw_body)
+        res = {
+            'success': False,
+            'error': "",
+            'data': data
+        }
+
+        conn = _engine.connect()
+        sku = conn.execute("SELECT rowid FROM product WHERE sku='{0}'".format(data['sku'])).fetchone()
+        if not sku:
+            conn.execute("INSERT INTO product VALUES ('{0}', '{1}')".format(data['sku'], data['description']))
+            res['success'] = True
+        else:
+            res['error'] = "Duplicate data"
+        conn.close()
+
+        return res
+
+    @cherrypy.tools.accept(media='text/plain')
+    @cherrypy.tools.json_out()
+    def DELETE(self, product_id):
+        res = {
+            'success': False,
+            'error': "",
+            'data': product_id
+        }
+
+        conn = _engine.connect()
+        n = conn.execute("SELECT rowid FROM cart WHERE product_id='{0}'".format(product_id)).fetchone()
+        if not n:
+            n = conn.execute("DELETE FROM product WHERE sku='{0}'".format(product_id)).rowcount
+            if n:
+                res['success'] = True
+            else:
+                res['error'] = "Can not delete"
+            conn.close()
+        else:
+            res['error'] = "Can not delete"
+
+        return res
+
+
+@cherrypy.expose
+class Cart(object):
+    @cherrypy.tools.accept(media='text/plain')
+    @cherrypy.tools.json_out()
+    def GET(self):
+        conn = _engine.connect()
+        res = conn.execute("SELECT * FROM cart").fetchall()
+        carts = {}
+        for item in res:
+            if item[0] in carts.keys():
+                carts[item[0]].append(item[1])
+            else:
+                carts[item[0]] = [item[1]]
+        return carts
+
+    @cherrypy.tools.accept(media='text/plain')
+    @cherrypy.tools.json_out()
+    def PUT(self):
+        cl = cherrypy.request.headers['Content-Length']
+        raw_body = cherrypy.request.body.read(int(cl))
+        data = json.loads(raw_body)
+        res = {
+            'success': False,
+            'error': "",
+            'data': data
+        }
+
+        conn = _engine.connect()
+        customer_id = conn.execute("SELECT rowid FROM customer WHERE rowid={0}".format(data['customer_id'])).fetchone()
+        if not customer_id:
+            res['error'] = "Can not find customer with this id"
+            conn.close()
+            return res
+        product_id = conn.execute("SELECT rowid FROM product WHERE sku='{0}'".format(data['sku'])).fetchone()
+        if not product_id:
+            res['error'] = "Can not find product with this id"
+            conn.close()
+            return res
+        item_id = conn.execute("SELECT rowid FROM cart WHERE customer_id='{0}' and product_id='{1}'"
+                               .format(data['customer_id'], data['sku'])).fetchone()
+        if item_id:
+            res['error'] = "This item already in the cart"
+            conn.close()
+            return res
+        conn.execute("INSERT INTO cart VALUES ('{0}', '{1}')".format(data['customer_id'], data['sku']))
+        res['success'] = True
+        conn.close()
+
+        return res
+
+    @cherrypy.tools.accept(media='text/plain')
+    @cherrypy.tools.json_out()
+    def DELETE(self, customer_id, product_id):
+        res = {
+            'success': False,
+            'error': "",
+            'data': (customer_id, product_id)
+        }
+
+        conn = _engine.connect()
+        n = conn.execute("DELETE FROM cart WHERE customer_id='{0}' AND product_id='{1}'"
+                         .format(customer_id, product_id)).rowcount
+        if n:
+            res['success'] = True
+        else:
+            res['error'] = "Can not delete"
+        conn.close()
+
+        return res
+
+
+def setup_database():
+    """
+    Create the `user_string` table in the database
+    on server startup
+    """
+
+    conn = _engine.connect()
+    conn.execute("CREATE TABLE product (sku, description)")
+    conn.execute("CREATE TABLE customer (first_name, last_name)")
+    conn.execute("CREATE TABLE cart (customer_id, product_id)")
+    conn.close()
+
+
+def cleanup_database():
+    """
+    Destroy the `user_string` table from the database
+    on server shutdown.
+    """
+
+    conn = _engine.connect()
+    conn.execute("DROP TABLE product")
+    conn.execute("DROP TABLE customer")
+    conn.execute("DROP TABLE cart")
+    conn.close()
+
+
+if __name__ == "__main__":
+    conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.sessions.on': True,
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        }
+    }
+
+    cherrypy.engine.subscribe('start', setup_database)
+    cherrypy.engine.subscribe('stop', cleanup_database)
+
+    cherrypy.tree.mount(SKU(), '/{0}/sku'.format(SECURE_STRING), conf)
+    cherrypy.tree.mount(Customer(), '/{0}/customer'.format(SECURE_STRING), conf)
+    cherrypy.tree.mount(Cart(), '/{0}/cart'.format(SECURE_STRING), conf)
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
---
+++ test_sales_service.py
@@ -0,0 +1,572 @@
+import json
+
+import requests
+from nose.tools import assert_equal
+
+from sales_service import SECURE_STRING
+
+
+def test_request_response():
+    # Send a request to the API server and store the response.
+    s = requests.Session()
+
+    data = {
+        "sku": "test",
+        "description": "just a test product"
+    }
+
+    # data = json.dumps(data)
+    r = s.put('http://127.0.0.1:8080/{0}/sku'.format(SECURE_STRING), data=json.dumps(data))
+
+    # Confirm that the request-response cycle completed successfully.
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': data
+    }
+
+    data = json.dumps(data)
+    assert_equal((r.status_code, r.text), (200, data))
+
+    data = {
+        "sku": "test",
+        "description": "just a test product"
+    }
+
+    r = s.put('http://127.0.0.1:8080/{0}/sku'.format(SECURE_STRING), data=json.dumps(data))
+
+    # Confirm that the request-response cycle completed successfully.
+
+    data = {
+        'success': False,
+        'error': "Duplicate data",
+        'data': data
+    }
+
+    data = json.dumps(data)
+    assert_equal((r.status_code, r.text), (200, data))
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': "test"
+    }
+    data = json.dumps(data)
+    r = s.delete('http://127.0.0.1:8080/{0}/sku/{1}'.format(SECURE_STRING, "test"))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    data = {
+        'success': False,
+        'error': "Can not delete",
+        'data': "test"
+    }
+    data = json.dumps(data)
+    r = s.delete('http://127.0.0.1:8080/{0}/sku/{1}'.format(SECURE_STRING, "test"))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    data = {
+        'success': False,
+        'error': "Can not delete",
+        'data': "test1"
+    }
+    data = json.dumps(data)
+    r = s.delete('http://127.0.0.1:8080/{0}/sku/{1}'.format(SECURE_STRING, "test1"))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    data = {
+        "first_name": "test",
+        "last_name": "test"
+    }
+
+    r = s.put('http://127.0.0.1:8080/{0}/customer'.format(SECURE_STRING), data=json.dumps(data))
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': data,
+        'id': 1
+    }
+    data = json.dumps(data)
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    r = s.delete('http://127.0.0.1:8080/{0}/customer/{1}'.format(SECURE_STRING, '1'))
+
+    # Confirm that the request-response cycle completed successfully.
+    data = {
+        'success': True,
+        'error': "",
+        'data': "1"
+    }
+
+    assert_equal((r.status_code, r.text), (200, json.dumps(data)))
+
+    r = s.get('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, u'{}'))
+
+    data = {
+        "customer_id": 1,
+        "sku": "test"
+    }
+
+    r = s.put('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING), data=json.dumps(data))
+
+    data = {
+        'success': False,
+        'error': "Can not find customer with this id",
+        'data': data
+    }
+
+    data = json.dumps(data)
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    r = s.get('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, u'{}'))
+
+    data = {
+        "first_name": "test",
+        "last_name": "test"
+    }
+
+    r = s.put('http://127.0.0.1:8080/{0}/customer'.format(SECURE_STRING), data=json.dumps(data))
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': data,
+        'id': 1
+    }
+    data = json.dumps(data)
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    data = {
+        "customer_id": 1,
+        "sku": "test"
+    }
+
+    r = s.put('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING), data=json.dumps(data))
+
+    data = {
+        'success': False,
+        'error': "Can not find product with this id",
+        'data': data
+    }
+
+    data = json.dumps(data)
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    r = s.get('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, u'{}'))
+
+    data = {
+        "sku": "test",
+        "description": "just a test product"
+    }
+
+    # data = json.dumps(data)
+    r = s.put('http://127.0.0.1:8080/{0}/sku'.format(SECURE_STRING), data=json.dumps(data))
+
+    # Confirm that the request-response cycle completed successfully.
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': data
+    }
+
+    data = json.dumps(data)
+    assert_equal((r.status_code, r.text), (200, data))
+
+    data = {
+        "customer_id": 1,
+        "sku": "test"
+    }
+
+    r = s.put('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING), data=json.dumps(data))
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': data
+    }
+
+    data = json.dumps(data)
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    r = s.get('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, u'{"1": ["test"]}'))
+
+    data = {
+        "sku": "test2",
+        "description": "just a test product"
+    }
+
+    # data = json.dumps(data)
+    r = s.put('http://127.0.0.1:8080/{0}/sku'.format(SECURE_STRING), data=json.dumps(data))
+
+    # Confirm that the request-response cycle completed successfully.
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': data
+    }
+
+    data = json.dumps(data)
+    assert_equal((r.status_code, r.text), (200, data))
+
+    data = {
+        "customer_id": 1,
+        "sku": "test2"
+    }
+
+    r = s.put('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING), data=json.dumps(data))
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': data
+    }
+
+    data = json.dumps(data)
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    r = s.get('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, u'{"1": ["test", "test2"]}'))
+
+    data = {
+        "customer_id": 1,
+        "sku": "test"
+    }
+
+    r = s.put('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING), data=json.dumps(data))
+
+    data = {
+        'success': False,
+        'error': "This item already in the cart",
+        'data': data
+    }
+
+    data = json.dumps(data)
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    data = {
+        'success': False,
+        'error': "Can not delete",
+        'data': "test"
+    }
+    data = json.dumps(data)
+    r = s.delete('http://127.0.0.1:8080/{0}/sku/{1}'.format(SECURE_STRING, "test"))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    r = s.delete('http://127.0.0.1:8080/{0}/cart/{1}/{2}'.format(SECURE_STRING, 1, 'test'))
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': ('1', 'test')
+    }
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, json.dumps(data)))
+
+    r = s.delete('http://127.0.0.1:8080/{0}/cart/{1}/{2}'.format(SECURE_STRING, 1, 'test2'))
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': ('1', 'test2')
+    }
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, json.dumps(data)))
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': "test2"
+    }
+    data = json.dumps(data)
+    r = s.delete('http://127.0.0.1:8080/{0}/sku/{1}'.format(SECURE_STRING, "test2"))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    r = s.delete('http://127.0.0.1:8080/{0}/cart/{1}/{2}'.format(SECURE_STRING, 1, 'test'))
+
+    data = {
+        'success': False,
+        'error': "Can not delete",
+        'data': ('1', 'test')
+    }
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, json.dumps(data)))
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': "test"
+    }
+    data = json.dumps(data)
+    r = s.delete('http://127.0.0.1:8080/{0}/sku/{1}'.format(SECURE_STRING, "test"))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    r = s.delete('http://127.0.0.1:8080/{0}/customer/{1}'.format(SECURE_STRING, '1'))
+
+    # Confirm that the request-response cycle completed successfully.
+    data = {
+        'success': True,
+        'error': "",
+        'data': "1"
+    }
+
+    assert_equal((r.status_code, r.text), (200, json.dumps(data)))
+
+    # ==================================================
+    # Deleting a customer while products in the cart
+    # ==================================================
+
+    r = s.get('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, u'{}'))
+
+    data = {
+        "customer_id": 1,
+        "sku": "test"
+    }
+
+    r = s.put('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING), data=json.dumps(data))
+
+    data = {
+        'success': False,
+        'error': "Can not find customer with this id",
+        'data': data
+    }
+
+    data = json.dumps(data)
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    r = s.get('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, u'{}'))
+
+    data = {
+        "first_name": "test",
+        "last_name": "test"
+    }
+
+    r = s.put('http://127.0.0.1:8080/{0}/customer'.format(SECURE_STRING), data=json.dumps(data))
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': data,
+        'id': 1
+    }
+    data = json.dumps(data)
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    data = {
+        "customer_id": 1,
+        "sku": "test"
+    }
+
+    r = s.put('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING), data=json.dumps(data))
+
+    data = {
+        'success': False,
+        'error': "Can not find product with this id",
+        'data': data
+    }
+
+    data = json.dumps(data)
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    r = s.get('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, u'{}'))
+
+    data = {
+        "sku": "test",
+        "description": "just a test product"
+    }
+
+    # data = json.dumps(data)
+    r = s.put('http://127.0.0.1:8080/{0}/sku'.format(SECURE_STRING), data=json.dumps(data))
+
+    # Confirm that the request-response cycle completed successfully.
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': data
+    }
+
+    data = json.dumps(data)
+    assert_equal((r.status_code, r.text), (200, data))
+
+    data = {
+        "customer_id": 1,
+        "sku": "test"
+    }
+
+    r = s.put('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING), data=json.dumps(data))
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': data
+    }
+
+    data = json.dumps(data)
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    r = s.get('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, u'{"1": ["test"]}'))
+
+    data = {
+        "sku": "test2",
+        "description": "just a test product"
+    }
+
+    # data = json.dumps(data)
+    r = s.put('http://127.0.0.1:8080/{0}/sku'.format(SECURE_STRING), data=json.dumps(data))
+
+    # Confirm that the request-response cycle completed successfully.
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': data
+    }
+
+    data = json.dumps(data)
+    assert_equal((r.status_code, r.text), (200, data))
+
+    data = {
+        "customer_id": 1,
+        "sku": "test2"
+    }
+
+    r = s.put('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING), data=json.dumps(data))
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': data
+    }
+
+    data = json.dumps(data)
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    r = s.get('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, u'{"1": ["test", "test2"]}'))
+
+    data = {
+        "customer_id": 1,
+        "sku": "test"
+    }
+
+    r = s.put('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING), data=json.dumps(data))
+
+    data = {
+        'success': False,
+        'error': "This item already in the cart",
+        'data': data
+    }
+
+    data = json.dumps(data)
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    r = s.delete('http://127.0.0.1:8080/{0}/customer/{1}'.format(SECURE_STRING, '1'))
+
+    # Confirm that the request-response cycle completed successfully.
+    data = {
+        'success': True,
+        'error': "",
+        'data': "1"
+    }
+
+    assert_equal((r.status_code, r.text), (200, json.dumps(data)))
+
+    r = s.get('http://127.0.0.1:8080/{0}/cart'.format(SECURE_STRING))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, u'{}'))
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': "test"
+    }
+    data = json.dumps(data)
+    r = s.delete('http://127.0.0.1:8080/{0}/sku/{1}'.format(SECURE_STRING, "test"))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+    data = {
+        'success': True,
+        'error': "",
+        'data': "test2"
+    }
+    data = json.dumps(data)
+    r = s.delete('http://127.0.0.1:8080/{0}/sku/{1}'.format(SECURE_STRING, "test2"))
+
+    # Confirm that the request-response cycle completed successfully.
+    assert_equal((r.status_code, r.text), (200, data))
+
+
+if __name__ == "__main__":
+    test_request_response()
